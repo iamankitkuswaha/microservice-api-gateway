91a8b84518472e423f69fa3d156746e718767e05
iamankitkuswaha <ankit21010@iiitd.ac.in>
2024-08-26 14:36:50 +0530

    Initial commit

---
+++ src/main/java/com/ankit/microservice_api_gateway/MicroserviceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.microservice_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApiGatewayApplication.class, args);
+	}
+
+}
